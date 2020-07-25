b70121176c88656583c75672e5ffe5af2f6a6800
Ignacio Amat <iamatperez@gmail.com>
2020-07-25 02:56:48 +0200

    Add face mask detection
    Tell if detected face is wearing mask protection or not

--- FaceDetection.py
+++ FaceDetection.py
@@ -5,6 +5,36 @@
 
 import cv2
 
+from tensorflow.keras.models import model_from_json
+import matplotlib.pyplot as plt
+import numpy as np
+import cv2
+
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.applications import MobileNetV2
+from tensorflow.keras.layers import Dense, MaxPooling2D, Flatten, Dropout, Conv2D, GlobalAveragePooling2D
+from tensorflow.keras.optimizers import Adam
+    
+with open('files\\model\\model_mask.json', 'r') as f:
+    loaded_model_json = f.read()
+model = model_from_json(loaded_model_json)
+
+model.load_weights("files\\weights\\model_mask.h5")
+print("Loaded model from disk")
+
+resMap = {
+        0 : 'Mask On',
+        1 : 'No Mask'
+    }
+
+colorMap = {
+        0 : (0,255,0),
+        1 : (0,0,255)
+    }
+
+def prepImg(pth):
+    return cv2.resize(pth,(224,224)).reshape(1,224,224,3)/255.0
+
 def faceDetectorImg(path):
     
     # Load the cascade
@@ -17,7 +47,11 @@ def faceDetectorImg(path):
     faces = face_cascade.detectMultiScale(gray, 1.1, 4)
     # Draw rectangle around the faces
     for (x, y, w, h) in faces:
-        cv2.rectangle(img, (x, y), (x+w, y+h), (255, 0, 0), 2)
+        #slicedImg = img[y:y+h,x:x+w]
+        pred = model.predict(prepImg(img))
+        pred = np.argmax(pred)
+        cv2.rectangle(img, (x, y), (x+w, y+h), colorMap[pred], 2)
+        cv2.putText(img, resMap[pred],(x,y-10),cv2.FONT_HERSHEY_SIMPLEX,0.8,(255,255,255),2) 
     # Display the output
     cv2.imshow('img', img)
     cv2.waitKey()
@@ -43,7 +77,11 @@ def faceDetectorVideo(path = ""):
         faces = face_cascade.detectMultiScale(gray, 1.1, 4)
         # Draw the rectangle around each face
         for (x, y, w, h) in faces:
-            cv2.rectangle(img, (x, y), (x+w, y+h), (255, 0, 0), 2)
+            #slicedImg = img[y:y+h,x:x+w]
+            pred = model.predict(prepImg(img))
+            pred = np.argmax(pred)
+            cv2.rectangle(img, (x, y), (x+w, y+h), colorMap[pred], 2)
+            cv2.putText(img, resMap[pred],(x,y-10),cv2.FONT_HERSHEY_SIMPLEX,0.8,(255,255,255),2) 
         # Display
         cv2.imshow('img', img)
         # Stop if escape key is pressed
@@ -53,9 +91,9 @@ def faceDetectorVideo(path = ""):
     # Release the VideoCapture object
     cap.release()
     cv2.destroyAllWindows()
-    
+      
 def main():
-    #faceDetectorImg('files\\images\\test.jpg')
+    faceDetectorImg('files\\images\\test.jpg')
     faceDetectorVideo()
 
 if __name__ == "__main__":
---
+++ FaceMaskTraining.py
@@ -0,0 +1,162 @@
+"""
+Face mask detection model
+Based on https://github.com/HOD101s/Face-Mask-Detection
+"""
+
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+import os
+import cv2
+import random
+import shutil
+
+import plotly.express as px
+import plotly.graph_objects as go
+import plotly.figure_factory as ff
+from plotly.subplots import make_subplots
+
+import tensorflow as tf
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.applications import MobileNetV2
+from tensorflow.keras.layers import Dense, MaxPooling2D, Flatten, Dropout, Conv2D, GlobalAveragePooling2D
+from tensorflow.keras.optimizers import Adam
+from tensorflow.keras.callbacks import ModelCheckpoint
+
+"""### Fetch data"""
+
+DATAPATH = 'files\\images\\face_mask_data'
+MASKPATH = 'files\\images\\face_mask_data\\with_mask'
+NOMASKPATH = 'files\\images\\face_mask_data\\without_mask'
+TESTPATH = 'files\\images\\test_face_mask_data'
+
+"""### Visualize data"""
+
+def view(pth):
+    images = list()
+    for img in random.sample(os.listdir(pth),9):
+        images.append(img)
+    i = 0
+    fig,ax = plt.subplots(nrows=3, ncols=3, figsize=(30,20))
+    for row in range(3):
+        for col in range(3):
+            ax[row,col].imshow(cv2.imread(os.path.join(pth,images[i])))
+            i+=1
+
+view(MASKPATH)
+
+view(NOMASKPATH)
+
+"""### Analyse data"""
+
+fig = go.Figure(
+    data=[go.Pie(labels=['WITHMASK','WITHOUTMASK'], 
+        values=[len(os.listdir(MASKPATH)),len(os.listdir(NOMASKPATH))])
+    ])
+fig.show()
+
+"""### Splitting data"""
+
+os.mkdir(TESTPATH)
+os.mkdir(os.path.join(TESTPATH,'with_mask'))
+os.mkdir(os.path.join(TESTPATH,'without_mask'))
+
+def getTest(pth):
+    dataSplit = int(np.ceil(len(os.listdir(pth))*0.02))
+    for img in os.listdir(pth)[-dataSplit:]:
+        shutil.move(os.path.join(pth,img), os.path.join('files\\images\\test_face_mask_datadata'.join(pth.split('files\\images\\face_mask_data')),img))
+getTest(MASKPATH)
+getTest(NOMASKPATH)
+
+len(os.listdir(MASKPATH)),len(os.listdir(NOMASKPATH))
+
+len(os.listdir(os.path.join(TESTPATH,'with_mask'))),len(os.listdir(os.path.join(TESTPATH,'without_mask')))
+
+"""### Prepare data input pipeline"""
+
+BATCH_SIZE = 32
+
+trainGen = ImageDataGenerator(
+    rescale= 1/255.,
+    horizontal_flip=True,
+    validation_split = 0.1
+)
+
+testGen = ImageDataGenerator(
+    rescale= 1/255.,
+)
+
+train = trainGen.flow_from_directory(
+    DATAPATH, 
+    target_size=(224, 224),
+    classes=['with_mask','without_mask'],
+    class_mode='categorical', 
+    batch_size=BATCH_SIZE, 
+    shuffle=True,
+    subset='training'
+)
+
+validation = trainGen.flow_from_directory(
+    DATAPATH, 
+    target_size=(224, 224),
+    classes=['with_mask','without_mask'],
+    class_mode='categorical', 
+    batch_size=BATCH_SIZE, 
+    shuffle=True,
+    subset='validation'
+)
+
+test = testGen.flow_from_directory(
+    TESTPATH, 
+    target_size=(224, 224), 
+    classes=['with_mask','without_mask'],
+    class_mode='categorical', 
+    batch_size=BATCH_SIZE, 
+    shuffle=True,
+)
+
+"""### Model building"""
+
+mob = MobileNetV2(
+    input_shape = (224,224,3),
+    include_top = False,
+    weights = 'imagenet',
+)
+mob.trainable = False
+
+model = Sequential()
+model.add(mob)
+model.add(GlobalAveragePooling2D())
+model.add(Dense(64,activation='relu'))
+model.add(Dropout(0.3))
+model.add(Dense(2,activation='softmax'))
+model.summary()
+
+
+# serialize model to JSON
+model_json = model.to_json()
+with open("files\\model\\model_mask.json", "w") as json_file:
+    json_file.write(model_json)
+
+model.compile(optimizer=Adam(),loss='categorical_crossentropy',metrics=['acc'])
+
+checkpoint = ModelCheckpoint(
+    'files\\weights\\model_mask.h5',
+    monitor='val_loss',
+    verbose=1,
+    save_best_only=True,
+    save_weights_only=True,
+    mode='min'
+)
+
+hist = model.fit(
+    train,
+    epochs = 15,
+    validation_data = validation,
+    callbacks = [checkpoint]
+)
+
+model.evaluate(test)
+
